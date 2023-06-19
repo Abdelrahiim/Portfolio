85266725c360c634b476a277e779a3379405dd26
Abdelrahiim <eng_abdelrhiim@yahoo.com>
2023-06-20 00:29:28 +0300

    adding Portfolio

--- src/main.ts
+++ src/main.ts
@@ -5,48 +5,35 @@ import "./Style/style.scss";
 const sections: NodeListOf<Element> =
   document.querySelectorAll(".section");
 
-const sectbtns  =
-  document.querySelectorAll(".controls");
+// const sectbtns =
+//   document.querySelectorAll(".controls");
 
-const sectbtn: NodeListOf<Element> =
+const sec_btn: NodeListOf<Element> =
   document.querySelectorAll(".control");
 
-const allSections:Element = document.querySelector(".main-content");
+
 
 const pageTransition = (): void => {
   // button click active class
-  sectbtn.forEach((btn) => {
-    btn.addEventListener("click", ():void => {
+  sec_btn.forEach((btn) => {
+    btn.addEventListener("click", (): void => {
       let currentBtn: NodeListOf<Element> = document.querySelectorAll(".active-btn");
-      
-      const id:string = btn.getAttribute('data-id');
-
       currentBtn[0].classList.remove('active-btn');
       btn.classList.add("active-btn");
-      sections.forEach((section)=>{
+      sections.forEach((section) => {
         section.classList.remove('active');
       })
-      const element:Element = document.getElementById(id);
-      element.classList.add('active')
-
+      const id = btn.getAttribute('data-id');
+      if (id) {
+        const element = document.getElementById(id);
+        if (element) {
+          element.classList.add('active');
+        }
+      }
     });
   });
-  // Sections Active Class
-//   allSections.addEventListener('click',(e:Event)=>{
-//     if (e.target instanceof Element){
-      
-
-//       // hide other Sections 
-      
-
-      
-//     }
-    
-    
-
-//   })
- };
+};
 
 pageTransition();
 
-export {};
+export { };
